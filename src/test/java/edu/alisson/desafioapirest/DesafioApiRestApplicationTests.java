9dcff2751f92bcb4d75b85d927b648fcbc482870
Alisson Rego <alissondroid07@gmail.com>
2023-10-06 08:53:50 -0300

    Primeiro commit

---
+++ src/test/java/edu/alisson/desafioapirest/DesafioApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.alisson.desafioapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
